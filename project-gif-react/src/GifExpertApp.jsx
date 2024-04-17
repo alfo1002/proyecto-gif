4ad0dbeaff091320ea435b0fd3fd28f99d46da8e
Walter Reyes <desarrolladorweb@grupofajardo.com.ec>
2024-04-16 22:42:34 -0500

    add: separando componente y comunicación P-H  y Emitir evento al Padre

--- project-gif-react/src/GifExpertApp.jsx
+++ project-gif-react/src/GifExpertApp.jsx
@@ -1,14 +1,28 @@
 import { useState } from "react"
+import { AddCategory } from "./components/AddCategory"
 
 export const GifExpertApp = () => {
 
-    const [categories, setcategories] = useState(['Games', 'Meme'])
-    console.log(categories)
+    const [categories, setCategories] = useState(['Games', 'Meme'])
+
+    const ondAddCategory = (newCategory) => {
+        console.log(newCategory)
+        //setCategories([...categories, 'verde'])
+        setCategories(cat => [...cat, newCategory])
+    }
+
     return (
         <>
             {/* Titulo */}
             <h1>GifExpertApp</h1>
-            <button>Agregar</button>
+
+            {/* Input */}
+            <AddCategory
+                //setCategories={setCategories} 
+                onNewCategory={value => ondAddCategory(value)}
+            />
+
+            {/*<button onClick={ondAddCategory}>Agregar</button> */}
             <ol>
                 {
                     categories.map(category => {
