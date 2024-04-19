f44569bcd5f26b06e2bdaffa3ca085fc3cedb9a8
Walter Reyes <desarrolladorweb@grupofajardo.com.ec>
2024-04-18 22:21:59 -0500

    add: usar useEffect para controlar renderización, desplegar localmente app con http-server

--- project-gif-react/src/GifExpertApp.jsx
+++ project-gif-react/src/GifExpertApp.jsx
@@ -7,24 +7,18 @@ export const GifExpertApp = () => {
     const [categories, setCategories] = useState(['Games',])
 
     const ondAddCategory = (newCategory) => {
-        console.log(newCategory)
         if (categories.includes(newCategory)) return
-        //setCategories([...categories, 'verde'])
         setCategories(cat => [...cat, newCategory])
     }
 
     return (
         <>
-            {/* Titulo */}
             <h1>GifExpertApp</h1>
 
-            {/* Input */}
             <AddCategory
-                //setCategories={setCategories} 
                 onNewCategory={value => ondAddCategory(value)}
             />
 
-            {/*<button onClick={ondAddCategory}>Agregar</button> */}
             {
                 categories.map(category => (
                     <GifGrid
