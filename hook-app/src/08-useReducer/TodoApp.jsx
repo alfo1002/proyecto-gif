27044d9c09ace0d23e625ddeba75d465b4640ffb
Walter Reyes <desarrolladorweb@grupofajardo.com.ec>
2024-05-22 22:47:43 -0500

    add: borrar elementos con useReducer y localstorage

--- hook-app/src/08-useReducer/TodoApp.jsx
+++ hook-app/src/08-useReducer/TodoApp.jsx
@@ -4,21 +4,16 @@ import { TodoList } from "./TodoList"
 import { TodoAdd } from "./TodoAdd"
 
 const initialState = [
-    /*{
-        id: new Date().getTime(),
-        desc: 'Aprender React',
-        done: false
-    },*/
-
+    /*{  id: new Date().getTime(),  desc: 'Aprender React',  done: false },*/
 ]
+
+const init = () => {
+    return JSON.parse(localStorage.getItem('todos')) || []
+}
 
 export const TodoApp = () => {
 
-    const [todos, dispatch] = useReducer(todoReducer, initialState)
-
-    const init = () => {
-        return JSON.parse(localStorage.getItem('todos')) || []
-    }
+    const [todos, dispatch] = useReducer(todoReducer, initialState, init)
 
     useEffect(() => {
         localStorage.setItem('todos', JSON.stringify(todos))
@@ -32,26 +27,28 @@ export const TodoApp = () => {
         dispatch(action)
     }
 
+    const handleDeleteTodo = (id) => {
+        const action = {
+            type: '[TODO] Delete Todo',
+            payload: id
+        }
+        dispatch(action)
+    }
+
     return (
         <>
             <h1>TodoApp: 10 <small>pendientes: 2</small> </h1>
             <hr />
-
             <div className="row">
                 <div className="col-7">
-                    {/* TodoList */}
-                    <TodoList todos={todos} />
-                    {/* TodoList */}
+                    <TodoList todos={todos} onDeleteTodo={handleDeleteTodo} />
                 </div>
-
                 <div className="col-5">
                     <h4>Agregar TODO</h4>
                     <hr />
                     <TodoAdd onNewTodo={handleNewTodo} />
                 </div>
             </div>
-
-
         </>
 
     )
--- hook-app/src/08-useReducer/todoReducer.js
+++ hook-app/src/08-useReducer/todoReducer.js
@@ -2,6 +2,8 @@ export const todoReducer = (initialState = [], action) => {
     switch (action.type) {
         case '[TODO] Add Todo':
             return [...initialState, action.payload]
+        case '[TODO] Delete Todo':
+            return initialState.filter(todo => todo.id !== action.payload)
         default:
             return initialState
     }
