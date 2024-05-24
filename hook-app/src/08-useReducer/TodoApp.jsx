fb4f5125f529cc705a5fb341db317150930f9a17
Walter Reyes <desarrolladorweb@grupofajardo.com.ec>
2024-05-23 22:07:19 -0500

    add: marcar como terminado una tarea

--- hook-app/src/08-useReducer/TodoApp.jsx
+++ hook-app/src/08-useReducer/TodoApp.jsx
@@ -35,13 +35,20 @@ export const TodoApp = () => {
         dispatch(action)
     }
 
+    const handleToggleTodo = (id) => {
+        dispatch({
+            type: '[TODO] Toggle Todo',
+            payload: id
+        })
+    }
+
     return (
         <>
             <h1>TodoApp: 10 <small>pendientes: 2</small> </h1>
             <hr />
             <div className="row">
                 <div className="col-7">
-                    <TodoList todos={todos} onDeleteTodo={handleDeleteTodo} />
+                    <TodoList todos={todos} onDeleteTodo={handleDeleteTodo} onToggleTodo={handleToggleTodo} />
                 </div>
                 <div className="col-5">
                     <h4>Agregar TODO</h4>
